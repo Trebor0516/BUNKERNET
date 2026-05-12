e0c32ccfc5b5448f49818ec25eefb6ee095e7d51
RobertM. <147338884+Trebor0516@users.noreply.github.com>
2026-05-11 23:13:16 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -150,7 +150,14 @@ function actualizarUsuarios() {
 // ===============================
 // SERVER
 // ===============================
-server.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
     console.log("Servidor iniciado:");
-    console.log("http://localhost:3000");
+    console.log(`Puerto: ${PORT}`);
 });
+
+//server.listen(3000, () => {
+  //  console.log("Servidor iniciado:");
+  //  console.log("http://localhost:3000");
+//});
